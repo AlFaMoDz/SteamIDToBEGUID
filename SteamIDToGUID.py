3f319a62a7a5fe3572ea55fc6a7482bf5fad0151
AlFaMoDz <25107205+AlFaMoDz@users.noreply.github.com>
2019-05-10 00:32:02 +0200

    Initial commit

---
+++ SteamIDToGUID.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# SteamID To BattleEye GUID
+# Author: AlFaMoDz | Twitter: @AlFaMoDzZ | GitHub: /alfamodz
+
+import sys
+import binascii
+from hashlib import md5
+
+
+def uid2guid(steam_id):
+    temp = ['BE']
+    for x in xrange(8):
+        temp.append(chr(steam_id & 0xFF))
+        steam_id >>= 8
+    return md5(''.join(temp)).hexdigest()
+
+def main(argc, argv) :
+    if argc < 2:
+        print('Usage: %s SteamID' % argv[0])
+        return 1
+
+    SteamID = int(argv[1])
+    print('|--------------------------------------------------|')
+    print('|       ~  SteamID to GUID by AlFaMoDz ~           |')
+    print('| ~ github.com/alfamodz - twitter.com/alfamodzz ~  |')
+    print('|--------------------------------------------------|\n')
+    print(' SteamID: %d \n GUID: %s' % (SteamID, uid2guid(SteamID)))
+
+if __name__ == '__main__':
+        sys.exit(main(len(sys.argv), sys.argv))
