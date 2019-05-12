d6a0b1fa29fff31f02843affd513d8377f3d803d
AlFaMoDz <25107205+AlFaMoDz@users.noreply.github.com>
2019-05-12 16:07:18 +0200

    Added support to massively convert SteamIDs from a .txt file.

--- SteamIDToGUID.py
+++ SteamIDToGUID.py
@@ -14,17 +14,28 @@ def uid2guid(steam_id):
         steam_id >>= 8
     return md5(''.join(temp)).hexdigest()
 
-def main(argc, argv) :
+def main(argc, argv):
     if argc < 2:
-        print('Usage: %s SteamID' % argv[0])
+        print('Usage: %s SteamID or .txt with IDs' % argv[0])
         return 1
 
-    SteamID = int(argv[1])
+    SteamID = argv[1]
     print('|--------------------------------------------------|')
     print('|       ~  SteamID to GUID by AlFaMoDz ~           |')
     print('| ~ github.com/alfamodz - twitter.com/alfamodzz ~  |')
     print('|--------------------------------------------------|\n')
-    print(' SteamID: %d \n GUID: %s' % (SteamID, uid2guid(SteamID)))
+    
+    if (argv[1].isdigit()):
+        print(' %s - %s' % (SteamID, uid2guid(int(SteamID))))
+    else: 
+        with open(argv[1]) as f:
+            array = []
+            for line in f:
+                array.append(line)
+        array = [x.strip() for x in array] 
+        
+        for x in range (0, (len(array))):
+            print(' %s - %s \n' % (array[x],uid2guid(int(array[x]))))
 
 if __name__ == '__main__':
-        sys.exit(main(len(sys.argv), sys.argv))
+    sys.exit(main(len(sys.argv), sys.argv))
